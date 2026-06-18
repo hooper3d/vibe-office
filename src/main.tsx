3e20a15160e7973ba67081ae0a1d0a2125bcfb09
hooper3d <8131324@qq.com>
2026-06-18 15:59:41 +0800

    Canonicalize localhost app URL

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,19 @@ import { createRoot } from "react-dom/client";
 import { App } from "./App";
 import "./styles.css";
 
-createRoot(document.getElementById("root")!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-);
+function redirectLocalhostToLoopback() {
+  if (window.location.hostname !== "localhost") return false;
+
+  const { protocol, port, pathname, search, hash } = window.location;
+  const canonicalUrl = `${protocol}//127.0.0.1${port ? `:${port}` : ""}${pathname}${search}${hash}`;
+  window.location.replace(canonicalUrl);
+  return true;
+}
+
+if (!redirectLocalhostToLoopback()) {
+  createRoot(document.getElementById("root")!).render(
+    <StrictMode>
+      <App />
+    </StrictMode>,
+  );
+}
